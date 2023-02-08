1ef4da21297eea0382c9c49ac08586d961d209db
Shlok <81300534+Shlok2@users.noreply.github.com>
2023-02-09 00:55:09 +0530

    Add files via upload

---
+++ client/build/precache-manifest.4261ff39aac5aa94151d9bfcace9fe4f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ea767fe2286dcc001e8c116ee6265cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eada38bdf73a3177d0da",
+    "url": "/static/css/main.dfaedcb8.chunk.css"
+  },
+  {
+    "revision": "7b8495c46ea8bb373396",
+    "url": "/static/js/2.859bd363.chunk.js"
+  },
+  {
+    "revision": "eca373d6688a5d5efead34244f081e8b",
+    "url": "/static/js/2.859bd363.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eada38bdf73a3177d0da",
+    "url": "/static/js/main.c83ceaa7.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.c83ceaa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  },
+  {
+    "revision": "c99936837e489b7e81c5419c8cfbebaa",
+    "url": "/static/media/404_error.c9993683.jpg"
+  },
+  {
+    "revision": "5b53f70c1b7cb2dbb9731ce612f3684d",
+    "url": "/static/media/destination.5b53f70c.png"
+  },
+  {
+    "revision": "6c7b5ec0184b26d4b93f36ec813156b3",
+    "url": "/static/media/memories-Text.6c7b5ec0.png"
+  }
+]);
